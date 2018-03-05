098259146c587655542396e1c2e7f803b267b129
Padeon <o5mariet@enib.fr>
2018-03-05 17:25:18 +0100

    Disable debug

--- web/index.php
+++ web/index.php
@@ -5,8 +5,6 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app['debug'] = true;
-
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
 	'twig.path' => __DIR__.'/views',
 ));
